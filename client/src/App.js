87f8c1806453aaef2b9fcdf074a14b7251d21eb3
Kristen Prescott <prescottdarling@gmail.com>
2021-06-23 13:28:32 -0400

    mostly stylistic changes; same functionality as prev commit, added prev/next buttons for future functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,17 @@ import axios from "axios";
 function App() {
   const [calendarData, setCalendarData] = useState({});
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await axios.get(
-          "https://www.reddit.com/r/animecalendar/new/.json?&limit=10"
-        );
-        // console.log(res.data);
-        const dateData = res.data;
-        setCalendarData(dateData);
-      } catch (err) {
-        console.error(err);
-      }
-      console.log("cal data: ", calendarData);
+  useEffect(async () => {
+    try {
+      const res = await axios(
+        "https://www.reddit.com/r/animecalendar/new/.json?&limit=10&raw_json=1"
+      );
+      console.log("calendar data: ", res.data);
+      const dateData = res.data;
+      setCalendarData(dateData);
+    } catch (err) {
+      console.error(err);
     }
-
-    fetchData();
   }, []);
 
   return (
@@ -29,18 +24,37 @@ function App() {
         className="App-header"
         style={{ textAlign: "center", padding: ".5rem" }}
       >
-        <h1>Anime Calendar</h1>
-        <hr style={{ width: "99vw" }}></hr>
+        <h1 className="title">Anime Calendar</h1>
+        {/* <hr style={{ width: "99vw" }}></hr> */}
         <main>
-          {/* <div>{calendarData && calendarData.data.dist}</div> */}
-          <div className="title">
-            {calendarData && calendarData.data.children[0].data.title}
-          </div>
-          <div className="img-wrapper">
-            <img
-              style={{ width: "80%" }}
-              src={calendarData && calendarData.data.children[0].data.url}
-            />
+          {/* <div className="date">
+            {calendarData.data && calendarData.data.children[0].data.title}
+          </div> */}
+          <div className="content-wrapper">
+            {/* <div>ウィークデイ</div> */}
+            <div className="words">ウィークエンド</div>
+
+            <div style={{ marginTop: "2rem" }} className="img-wrapper">
+              <img
+                className="image"
+                style={{ width: "80%", borderRadius: "4px" }}
+                src={
+                  calendarData.data && calendarData.data.children[0].data.url
+                }
+              />
+              <div className="arrow-btn-wrapper">
+                <div className="arrow-btn prev-btn">
+                  <a href={calendarData.data && calendarData.data.before}>
+                    &#5130;
+                  </a>
+                </div>
+                <div className="arrow-btn next-btn">
+                  <a href={calendarData.data && calendarData.data.after}>
+                    &#5125;
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </header>
